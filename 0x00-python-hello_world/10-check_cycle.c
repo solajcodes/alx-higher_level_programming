21d9a3fa42127de754ce8449bf7035c29880d2e1
solajcodes <olalekansunday638@gmail.com>
2023-01-02 13:19:03 -0800

    A function that checks for a cycle in a singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+			if (slow == fast)
+			{
+				return (1);
+			}
+	}
+
+	return (0);
+}
