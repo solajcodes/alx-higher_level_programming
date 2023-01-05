9d6077107ca75b0d0c5a73747b60352e524b7571
solajcodes <olalekansunday638@gmail.com>
2023-01-05 21:40:43 +0100

    command line calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,15 +3,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) -1 != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operator_dict = {"+": add, "*": mul, "-": sub, "/": div}
-    if sys.argv[2] not in list(operator_dict.keys()):
+    optr = {"+": add, "*": mul, "-": sub, "/": div}
+    if sys.argv[2] not in list(optr.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b,  operator_dict[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b,  optr[sys.argv[2]](a, b)))
