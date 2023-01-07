ba507bcfa9dfe3c4a2531ae1028caf45413d6403
solajcodes <olalekansunday638@gmail.com>
2023-01-07 21:46:28 +0100

    tuple indexing

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = len(sentence)
-    b = sentence[0]
-    return a, b
+    if len(sentence) == 0:
+        return None
+    else:
+        return len(sentence), sentence[0]
