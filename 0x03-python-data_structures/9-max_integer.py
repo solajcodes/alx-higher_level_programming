5e9f2fda8d681af4fac3fa20209acf3c36e19ea9
solajcodes <olalekansunday638@gmail.com>
2023-01-07 22:21:45 +0100

    using for loop to get the max integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_v = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for item in my_list:
+            if item > max_v:
+                max_v = item
+        return max_v
