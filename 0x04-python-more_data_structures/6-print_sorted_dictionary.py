ed1fdb4b0df0130f7012ee51c6d648a4d3a7b822
solajcodes <olalekansunday638@gmail.com>
2023-01-12 04:28:36 +0100

    printing keys and values in a dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    new_dic = a_dictionary
-    for i in sorted(new_dic):
-        print("{} : {}".format(i, new_dic[i]))
+def print_sorted_dictionary(my_dict):
+    for keys in sorted(my_dict.keys()):
+        print('{}: {}'.format(keys, my_dict[keys]))
