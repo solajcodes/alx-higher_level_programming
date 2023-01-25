9aec12d6c78565d9d61ae210cc8d9ad46cd3a282
solajcodes <olalekansunday638@gmail.com>
2023-01-25 04:38:55 +0100

    a square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,12 @@
 # 0-square.py
 """Defining a class square"""
 
+
 class Square:
     """Represent a Square"""
+
     def __init__(self, size):
         """initializing this square class
         Args: size - represents the size of the class Square defined"""
+
         self.__size = size
